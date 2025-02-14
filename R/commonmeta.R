ee94e9960a87480766f45e25aa36016dd61ce6e1
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-02-14 14:02:50 +0100

    docs: escape example

--- R/commonmeta.R
+++ R/commonmeta.R
@@ -5,7 +5,7 @@
 #' @return A random DOI string
 #' @export
 #'
-#' @examples
+#' @examplesIf interactive()
 #' commonmeta_doi()
 commonmeta_doi <- function(prefix = "10.59350") {
   # TODO: check this works on windows
